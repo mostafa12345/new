46ce338f1c37cea86e8be4fcaf4612f828be6000
mostafa12345 <129852848+mostafa12345@users.noreply.github.com>
2024-11-02 18:51:06 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,11 +10,11 @@ terraform {
   backend "s3" {
      bucket = "testasdawf4115154"
      key    = "terraform.tfstate"
-    region  = "us-east-1"
+     region = "us-east-1"
   }
 }
 
 # Configure aws provider
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
 }
